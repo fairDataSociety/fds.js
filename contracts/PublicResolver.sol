3f8c157a3b604e05ef8fdcc8f391f8a1d12d3bc0
significance <dan@1up.digital>
2019-09-09 15:49:21 +0100

    fix bugs

--- contracts/PublicResolver.sol
+++ contracts/PublicResolver.sol
@@ -63,7 +63,7 @@ contract PublicResolver {
      * May only be called by the owner of that node in the ENS registry.
      * @param node The node to update.
      * @param addr The address to set.
-	 * @param hash The content hash to set
+	 * @param content The content hash to set
 	 * @param multihash The multihash to set
      * @param x the X coordinate of the curve point for the public key.
      * @param y the Y coordinate of the curve point for the public key.
