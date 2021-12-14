307484e4799d31bcd01dfc4b066c2676b6262989
Manuel Casas Barrado <manuel.casasbarrado@gmail.com>
2021-12-14 14:15:20 +0100

    feat: update deployment configuration to support fairdrop.eth domains

--- migrations/1563788331_ens.js
+++ migrations/1563788331_ens.js
@@ -27,18 +27,18 @@ module.exports = function (deployer, network, accounts) {
       await deployer.deploy(PublicResolver, ens.address)
 
       await ens.setSubnodeOwner('0x0', '0x' + sha3('eth'), accounts[0])
-      await ens.setSubnodeOwner(namehash.hash('eth'), '0x' + sha3('datafund'), accounts[0])
+      await ens.setSubnodeOwner(namehash.hash('eth'), '0x' + sha3('fairdrop'), accounts[0])
 
       const resolver = await PublicResolver.deployed()
-      await ens.setResolver(namehash.hash('datafund.eth'), resolver.address)
+      await ens.setResolver(namehash.hash('fairdrop.eth'), resolver.address)
 
       const dhr = await TestRegistrar.deployed()
       await ens.setSubnodeOwner('0x0', '0x' + sha3('eth'), accounts[0])
 
-      await deployer.deploy(SubdomainRegistrar, ens.address, namehash.hash('datafund.eth'))
+      await deployer.deploy(SubdomainRegistrar, ens.address, namehash.hash('fairdrop.eth'))
 
       const registrar = await SubdomainRegistrar.deployed()
-      await ens.setSubnodeOwner(namehash.hash('eth'), '0x' + sha3('datafund'), registrar.address)
+      await ens.setSubnodeOwner(namehash.hash('eth'), '0x' + sha3('fairdrop'), registrar.address)
     } else {
       // const ens = ENS.deployed();
       // await deployer.deploy(SubdomainRegistrar, ens.address);
